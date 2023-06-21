9361a869db50787afd2b8678598212d5519b23ee
alex19992020 <alejandroantunez1999@gmail.com>
2023-06-20 23:36:24 -0700

    completed e-signature app

--- src/projects/EsignatureApp.jsx
+++ src/projects/EsignatureApp.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "./components/Title";
 
 export default function EsignatureApp() {
+  const [name, setName] = useState("Your Signature");
+  const [date, setDate] = useState("");
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+  };
   const inputStyle = {
     border: "none",
     borderBottom: "2px dotted",
@@ -11,8 +19,8 @@ export default function EsignatureApp() {
   document.body.style.background = "#eee";
   return (
     <div className="container text-center">
-      <Title classes={"title"} text={"Name"} />
-      <Title classes={"main-title mb-4"} text={"Date"} />
+      <Title classes={"title"} text={name} />
+      <Title classes={"main-title mb-4"} text={!date ? "DoB" : date} />
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione in
         architecto alias aut repudiandae, soluta necessitatibus, maxime
@@ -27,8 +35,18 @@ export default function EsignatureApp() {
           top: "40vh",
         }}
       >
-        <input type="date" value={""} style={inputStyle} />
-        <input type="text" value={""} style={inputStyle} />
+        <input
+          type="date"
+          value={date}
+          style={inputStyle}
+          onChange={handleDateChange}
+        />
+        <input
+          type="text"
+          value={name}
+          style={inputStyle}
+          onChange={handleNameChange}
+        />
       </footer>
     </div>
   );
